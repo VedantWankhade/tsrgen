b0874fabcedbd095188bd5292ad7abe18504fc5f
VedantWankhade <vedantwankhade17@gmail.com>
2024-06-14 23:45:50 +0530

    frontend: basic form and keep-alive router views

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <the-header></the-header>
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
   <the-footer></the-footer>
 </template>
 
--- frontend/src/views/GenerateView.vue
+++ frontend/src/views/GenerateView.vue
@@ -1,3 +1,58 @@
 <template>
-  <h1>Generate View</h1>
+  <section>
+    <div>
+      <h1>Generate TSR</h1>
+    </div>
+    <div>
+      <form @submit.prevent="submit">
+        <div>
+          <label for="atlassianInstance">Atlassian Instance Server</label>
+          <input
+            type="text"
+            name="atlassianInstance"
+            id="atlassianInstance"
+            v-model="atlassianInstance"
+          />
+        </div>
+        <div>
+          <label for="atlassianUsername">Atlassian Username</label>
+          <input
+            type="text"
+            name="atlassianUsername"
+            id="atlassianUsername"
+            v-model="atlassianUsername"
+          />
+        </div>
+        <div>
+          <label for="atlassianToken">Atlassian Token</label>
+          <textarea name="atlassianToken" id="atlassianToken" v-model="atlassianToken" />
+        </div>
+        <div>
+          <label for="jql">JQL</label>
+          <input type="text" name="jql" id="jql" v-model="jql" />
+        </div>
+        <div>
+          <button type="submit">Generate</button>
+        </div>
+      </form>
+    </div>
+  </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      atlassianInstance: '',
+      atlassianUsername: '',
+      atlassianToken: '',
+      jql: ''
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.atlassianInstance, this.atlassianUsername, this.atlassianToken, this.jql)
+    }
+  }
+}
+</script>
